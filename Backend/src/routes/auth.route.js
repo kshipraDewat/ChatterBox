178054f3c21c99e2367bcd6319428c55b77437f5
kshipraDewat <kshipradewat66@gmail.com>
2024-12-12 13:30:51 +0530

    signup and login backend

--- Backend/src/routes/auth.route.js
+++ Backend/src/routes/auth.route.js
@@ -1,11 +1,11 @@
-import express from 'express'
-import {  signup, login, logout } from '../controllers/auth.controller.js';
+import express from "express";
+import {  login, logout, signup } from "../controllers/auth.controller.js";
+
 const router = express.Router();
 
-router.get("/signup",signup)
+router.post("/signup", signup);
+router.post("/login", login);
+router.post("/logout", logout);
 
-router.get("/login", login)
-
-router.get("/logout",logout)
 
 export default router;
