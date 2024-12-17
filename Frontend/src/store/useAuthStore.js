1f908d40a44af34a8c6c18d48a724e7667c37ad4
kshipraDewat <kshipradewat66@gmail.com>
2024-12-17 11:24:27 +0530

    checking authentication

--- Frontend/src/store/useAuthStore.js
+++ Frontend/src/store/useAuthStore.js
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { axiosInstance } from "../lib/axios";
 
 
 export const useAuthStore = create((set)=>({
@@ -6,6 +7,19 @@ export const useAuthStore = create((set)=>({
     isSigningUp : false,
     isLoggingIn: false,
     isUpdatingProfile : false,
-    
+
     isCheckingAuth: true, 
+
+    chechAuth: async()=>{
+        try{
+            const res = await axiosInstance.get("/auth/check")
+
+            set({authUser:res.data})
+        }catch(error){
+            console.log("Error in checkAuth:", error)
+            set({authUser: null})
+        }finally{
+            set({isCheckingAuth:false})
+        }
+    }
 }))
